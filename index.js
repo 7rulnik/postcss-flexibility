c6f1ca9f870c42985b284b185d18c3fb4e4359ea
Alexandr Zhidovlenko <yozhezhi@gmail.com>
2021-05-30 23:35:39 +0300

    Migrate to PostCss 8 (#141)
    Co-authored-by: Alexandr Zhidovlenko <azhidovlenko@alfabank.ru>

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-import postcss from 'postcss';
+const postcssFlexibility = () => ({
+	postcssPlugin: 'postcss-flexibility',
+	Once(root) {
+		root.walkRules(rule => {
+			const isDisabled = rule.nodes.some(({prop, text = ''}) =>
+				prop === '-js-display' || text.endsWith('flexibility-disable'));
 
-const postcssFlexibility = postcss.plugin('postcss-flexibility', () => css => {
-	css.walkRules(rule => {
-		const isDisabled = rule.some(({prop, text = ''}) =>
-			prop === '-js-display' || text.endsWith('flexibility-disable')
-		);
-
-		if (!isDisabled) {
-			rule.walkDecls('display', decl => {
-				const {value} = decl;
-				if (/^flex$|^inline-flex$/.test(value)) {
-					decl.cloneBefore({prop: '-js-display'});
-				}
-			});
-		}
-	});
+			if (!isDisabled) {
+				rule.walkDecls('display', decl => {
+					const {value} = decl;
+					if (/^flex$|^inline-flex$/.test(value)) {
+						decl.cloneBefore({prop: '-js-display'});
+					}
+				});
+			}
+		});
+	}
 });
+
+postcssFlexibility.postcss = true;
 
 export default postcssFlexibility;
--- index.spec.js
+++ index.spec.js
@@ -3,7 +3,7 @@ import postcss from 'postcss';
 import plugin from './';
 
 function run(input, output) {
-	return postcss([plugin()]).process(input)
+	return postcss([plugin()]).process(input, {from: undefined})
 		.then(result => {
 			expect(result.css).toEqual(output);
 			expect(result.warnings().length).toEqual(0);
@@ -13,21 +13,25 @@ function run(input, output) {
 test('Add "-js-display: flex" if "display: flex" present', () => {
 	return run(
 		`a {
-            display: flex;
-        }`,
+			color: red;
+			display: flex;
+		}`,
 		`a {
-            -js-display: flex;
-            display: flex;
-        }`
+			color: red;
+			-js-display: flex;
+			display: flex;
+		}`
 	);
 });
 
 test('Add "-js-display: inline-flex" if "display: inline-flex" present', () => {
 	return run(
 		`a {
+			color: red;
 			display: inline-flex;
 		}`,
 		`a {
+			color: red;
 			-js-display: inline-flex;
 			display: inline-flex;
 		}`
@@ -37,10 +41,12 @@ test('Add "-js-display: inline-flex" if "display: inline-flex" present', () => {
 test('Don\'t add "-js-display: flex" if it\'s already exist', () => {
 	return run(
 		`a {
+			color: red;
 			-js-display: flex;
 			display: flex;
 		}`,
 		`a {
+			color: red;
 			-js-display: flex;
 			display: flex;
 		}`
@@ -50,10 +56,12 @@ test('Don\'t add "-js-display: flex" if it\'s already exist', () => {
 test('Don\'t add "-js-display: flex" if comment "flexibility-disable" is exist', () => {
 	return run(
 		`a {
+			color: red;
 			/* flexibility-disable */
 			display: flex;
 		}`,
 		`a {
+			color: red;
 			/* flexibility-disable */
 			display: flex;
 		}`
@@ -63,10 +71,12 @@ test('Don\'t add "-js-display: flex" if comment "flexibility-disable" is exist',
 test('Don\'t add "-js-display: flex" if comment "! flexibility-disable" is exist', () => {
 	return run(
 		`a {
+			color: red;
 			/*! flexibility-disable */
 			display: flex;
 		}`,
 		`a {
+			color: red;
 			/*! flexibility-disable */
 			display: flex;
 		}`
@@ -76,22 +86,55 @@ test('Don\'t add "-js-display: flex" if comment "! flexibility-disable" is exist
 test('Don\'t add "-js-display: flex" for prefixed version', () => {
 	return run(
 		`a {
+			color: red;
 			display: -webkit-flex;
 			display: flex;
 		}
 		b {
+			color: red;
 			display: -moz-inline-flex;
 			display: inline-flex;
 		}`,
 		`a {
+			color: red;
 			display: -webkit-flex;
 			-js-display: flex;
 			display: flex;
 		}
 		b {
+			color: red;
 			display: -moz-inline-flex;
 			-js-display: inline-flex;
 			display: inline-flex;
 		}`
 	);
+});
+
+test('Works with other PostCSS plugins', () => {
+	const changedSelector = 'bbb';
+
+	const otherPlugin = () => {
+		return {
+			postcssPlugin: 'test',
+			Rule(rule) {
+				rule.selector = changedSelector;
+			}
+		};
+	};
+	otherPlugin.postcss = true;
+
+	const result = postcss([otherPlugin(), plugin()]).process(
+		`aaa {
+			display: flex;
+		}`, {
+			from: undefined
+		});
+
+	expect(result.css).toEqual(
+		`${changedSelector} {
+			-js-display: flex;
+			display: flex;
+		}`
+	);
+	expect(result.warnings().length).toEqual(0);
 });
